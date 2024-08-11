a0e36671765ff6ea9dbfee3ee0aea7a650ef091f
jmetzg11alpine <jmetzg11@gmail.com>
2024-08-11 06:47:29 -0700

    made helper function for pie backend

--- api/src/budget/endpoints.rs
+++ api/src/budget/endpoints.rs
@@ -1,81 +1,25 @@
 use actix_web::{post, get, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use sqlx::MySqlPool;
+use sqlx::query_as;
 use serde_json::json;
-use crate::budget::data::{get_renaming_map, get_colors};
-
-
-#[derive(Serialize)]
-struct AgencyEntry {
-    label: String,
-    value: f32,
-    tooltip: String,
-    background_color: String,
-}
+use crate::budget::helpers::{process_agency_data, AgencyBudget};
 
 
 #[get("/agency")]
 async fn get_agency(pool: web::Data<MySqlPool>) -> impl Responder {
-    let renaming = get_renaming_map();
-    let colors = get_colors();
 
-    let result = sqlx::query!("SELECT id, agency, budget FROM agency_budget").fetch_all(pool.get_ref()).await;
+    let result = query_as!(AgencyBudget, "Select agency, budget FROM agency_budget order by budget desc").fetch_all(pool.get_ref()).await;
 
     match result {
-        Ok(rows) => {
-            let mut main_data = Vec::new();
-            let mut other_data = Vec::new();
-            let mut main_other_value = 0.0;
-            let mut other_other_value = 0.0;
-            let mut other_other_labels = Vec::new();
-
-            for (i, row) in rows.iter().enumerate() {
-                let agency = row.agency.as_deref().unwrap_or("unknown agency");
-                let budget = row.budget.unwrap_or(0.0);
-                let label = renaming.get(agency).unwrap_or(&agency).to_string();
-                let background_color = format!("rgba({}, 1)", colors[i % colors.len()]);
-
-                if i < 9 {
-                    main_data.push(AgencyEntry{
-                        label,
-                        value: budget,
-                        tooltip: agency.to_string(),
-                        background_color: background_color
-                    });
-                } else if i < 18 {
-                    main_other_value += budget;
-                    other_data.push(AgencyEntry {
-                        label,
-                        value: budget,
-                        tooltip: agency.to_string(),
-                        background_color: background_color
-                    });
-                } else {
-                    main_other_value += budget;
-                    other_other_value += budget;
-                    other_other_labels.push((agency.to_string(), budget));
-                }
-            }
-
-            main_data.push(AgencyEntry {
-                label: ("other").to_string(),
-                value: main_other_value,
-                tooltip: ("break down in other graph").to_string(),
-                background_color: format!("rbga({}, 1)", colors[9])
-            });
-
-            other_data.push(AgencyEntry {
-                label: format!("{} others", other_other_labels.len()),
-                value: other_other_value,
-                tooltip: ("break down described below").to_string(),
-                background_color: format!("rbga({}, 1)", colors[9])
-            });
+      Ok(rows) => {
+        let (main_data, other_data, table_labels) = process_agency_data(&rows);
             HttpResponse::Ok().json(json!({
                 "main_data": main_data,
                 "other_data": other_data,
-                "other_other_labels": other_other_labels
+                "table_labels": table_labels
             }))
-        }
+    }
          Err(e) => {
             eprintln!("Failed to execute query: {}", e);
             HttpResponse::InternalServerError().json(json!({"error": "Failed to retrieve data"}))
--- api/src/budget/helpers.rs
+++ api/src/budget/helpers.rs
@@ -1,1 +1,71 @@
+use crate::budget::data::{get_colors, get_renaming_map};
+use serde::Serialize;
 
+#[derive(Debug)]
+pub struct AgencyBudget {
+    pub agency: Option<String>,
+    pub budget: Option<f32>,
+}
+
+#[derive(Serialize)]
+pub struct AgencyEntry {
+    label: String,
+    value: f32,
+    tooltip: String,
+    background_color: String,
+}
+
+pub fn process_agency_data(
+    rows: &[AgencyBudget],
+) -> (Vec<AgencyEntry>, Vec<AgencyEntry>, Vec<(String, f32)>) {
+    let renaming = get_renaming_map();
+    let colors = get_colors();
+
+    let mut main_data = Vec::new();
+    let mut other_data = Vec::new();
+    let mut main_other_value = 0.0;
+    let mut other_other_value = 0.0;
+    let mut table_labels = Vec::new();
+
+    for (i, row) in rows.iter().enumerate() {
+        let agency = row.agency.as_deref().unwrap_or("unknown agency");
+        let budget = row.budget.unwrap_or(0.0);
+        let label = renaming.get(agency).unwrap_or(&agency).to_string();
+
+        if i < 9 {
+            main_data.push(AgencyEntry {
+                label,
+                value: budget,
+                tooltip: agency.to_string(),
+                background_color: format!("rgba({}, 1)", colors[i]),
+            })
+        } else if i < 18 {
+            main_other_value += budget;
+            other_data.push(AgencyEntry {
+                label,
+                value: budget,
+                tooltip: agency.to_string(),
+                background_color: format!("rgba({}, 1)", colors[i - 9]),
+            })
+        } else {
+            main_other_value += budget;
+            other_other_value += budget;
+            table_labels.push((agency.to_string(), budget));
+        }
+    }
+    main_data.push(AgencyEntry {
+        label: ("other").to_string(),
+        value: main_other_value,
+        tooltip: ("break down in other graph").to_string(),
+        background_color: format!("rgba({}, 1)", colors[9]),
+    });
+
+    other_data.push(AgencyEntry {
+        label: format!("{} others", table_labels.len()),
+        value: other_other_value,
+        tooltip: ("break down described below").to_string(),
+        background_color: format!("rgba({}, 1)", colors[9]),
+    });
+
+    (main_data, other_data, table_labels)
+}
